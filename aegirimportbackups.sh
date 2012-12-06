0ebe3e7a7fc253b0ff67bbbe8acf2bda950ab013
o1 <o1@sites.rentpingmedia.com>
2012-12-06 10:08:32 -0600

    update for defining aegir client

--- aegirimportbackups.sh
+++ aegirimportbackups.sh
@@ -9,8 +9,8 @@
 _TMPFOLDER="/var/aegir/tmp/migrate" #Absolute path to directory where you backups are located, No backslash at the end
 _INSTPROFILE="default" #if none, use the word: default
 _PLATFORM="@platform_STSBase62014p" #include @ before it
-# this does nothing_BACKUPYEAR="2011"
-# YOU NEED TO CHANGE THE YEAR 
+_CLIENT="admin" #admin is the usual client, change this if you want to associate the site with a different client
+# YOU NEED TO CHANGE THE YEAR down below on line 36
 
 
 
@@ -37,7 +37,7 @@ do
   _FILENAME="$f"
   echo ''
   msg "INFO: Working on site $_URL"
-  drush provision-save @$_URL --context_type=site --platform=$_PLATFORM --profile=$_INSTPROFILE --uri=$_URL db_server=@server_localhost --client_name=admin
+  drush provision-save @$_URL --context_type=site --platform=$_PLATFORM --profile=$_INSTPROFILE --uri=$_URL db_server=@server_localhost --client_name=$_CLIENT
   drush @$_URL provision-deploy $_TMPFOLDER/$_FILENAME
   echo ''
   echo ''
